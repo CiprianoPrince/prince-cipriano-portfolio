851cf594af8cc7d48861c50fefc8656ae9f2a308
Maricris De Jesus <maricris052298dejesus@gmail.com>
2024-07-19 11:17:30 +0800

    updated intro + updated image portrait

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,8 +31,8 @@ export default function Intro() {
                         }}
                     >
                         <Image
-                            src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-                            alt="Ricardo portrait"
+                            src="https://raw.githubusercontent.com/CiprianoPrince/codepen-assets/main/Prince150x150.jpg"
+                            alt="Princeton portrait"
                             width="192"
                             height="192"
                             quality="95"
@@ -62,11 +62,11 @@ export default function Intro() {
                 initial={{ opacity: 0, y: 100 }}
                 animate={{ opacity: 1, y: 0 }}
             >
-                <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{' '}
-                <span className="font-bold">full-stack developer</span> with{' '}
-                <span className="font-bold">8 years</span> of experience. I enjoy building{' '}
+                <span className="font-bold">Hello, I'm Princeton.</span> I'm a{' '}
+                <span className="font-bold">full-stack web developer</span> with{' '}
+                <span className="font-bold">1 year</span> of experience. I enjoy building{' '}
                 <span className="italic">sites & apps</span>. My focus is{' '}
-                <span className="underline">React (Next.js)</span>.
+                <span className="underline">React and Angular</span>.
             </motion.h1>
 
             <motion.div
--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: true,
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com',
+            },
+        ],
+    },
+    experimental: {
+        serverActions: true,
+    },
 };
 
 module.exports = nextConfig;
