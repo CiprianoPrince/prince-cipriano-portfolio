7685a614645171954632b67ebc848887e588414b
Maricris De Jesus <maricris052298dejesus@gmail.com>
2024-07-19 12:01:54 +0800

    updated experience and skills and email

--- components/about.tsx
+++ components/about.tsx
@@ -18,7 +18,7 @@ export default function About() {
             id="about"
         >
             <SectionHeading>About me</SectionHeading>
-            <p className="mb-3">
+            {/* <p className="mb-3">
                 After graduating with a degree in <span className="font-medium">Accounting</span>, I
                 decided to pursue my passion for programming. I enrolled in a coding bootcamp and
                 learned <span className="font-medium">full-stack web development</span>.{' '}
@@ -37,6 +37,35 @@ export default function About() {
                 <span className="font-medium">learning new things</span>. I am currently learning
                 about <span className="font-medium">history and philosophy</span>. I'm also learning
                 how to play the guitar.
+            </p> */}
+
+            <p className="mb-3">
+                After graduating with a degree in{' '}
+                <span className="font-medium">Information Technology</span>, I decided to further my
+                programming skills by enrolling in a{' '}
+                <span className="font-medium">3-month full-stack web development bootcamp</span>,
+                where I graduated as the <span className="font-medium">top student</span>. My core
+                stack includes{' '}
+                <span className="font-medium">React, Express, Angular, and Nest</span>.{' '}
+                <span className="italic">I am passionate about programming</span>, especially the
+                problem-solving aspect, and I <span className="underline">enjoy</span> the
+                satisfaction of finding solutions to challenging problems.
+            </p>
+
+            <p>
+                <span className="italic">In my free time</span>, I love binge-watching{' '}
+                <span className="font-medium">cybersecurity or networking content</span> and reading
+                books about programming, such as <span className="font-medium">"Clean Code"</span>{' '}
+                and various JavaScript topics. I am always eager to learn new technologies and
+                expand my knowledge. Currently, I am seeking a{' '}
+                <span className="font-medium">full-time position</span> as a software developer.
+            </p>
+
+            <p>
+                <span className="italic">Outside of coding</span>
+                watching movies and animes, reading manga. I also have a keen interest in{' '}
+                <span className="font-medium">learning new things</span>, such as history and I'm
+                currently teaching myself how to play the guitar.
             </p>
         </motion.section>
     );
